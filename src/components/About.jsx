24e53cf49a601db6a79a7c06a8c3fc6ac73b853e
Demain <demainkkinc@gmail.com>
2025-05-10 13:12:50 -0700

    Updated about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
         </div>
         <div className='w-[45%] my-auto'>
             <h5 className='text-gray-600 text-[20px] font-semibold'>Who we are</h5>
-            <h1 className='text-[30px] font-semibold'>Get to know abouts <span className='text-[#5c07B7]'>Splittas <br /> Technology.</span></h1>
+            <h1 className='text-[30px] font-semibold'>Get to know aboutsq <span className='text-[#5c07B7]'>Splittas <br /> Technology.</span></h1>
             <p className='text-[16px] text-gray-600 font-normal text-sm/6 w-[80%] mt-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque deserunt dignissimos ipsum labore non dolor dolore animi quibusdam repellat perspiciatis expedita enim saepe assumenda vitae pariatur provident esse, itaque rerum.</p>
             <button className='w-[120px] h-[37px] bg-[#5c07B7] rounded-3xl text-white font-semibold text-[14px] mt-8'>Read more <FontAwesomeIcon className='-rotate-45' icon={faArrowRightLong} /></button>
         </div>
